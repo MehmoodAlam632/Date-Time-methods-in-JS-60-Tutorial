2220ad5ce83af0f18aa15b15c2b9ed8cc4f99f20
mehmood <mehmoodalam60632@gmail.com>
2022-05-07 00:27:39 +0500

    Math & Date methods in Javascript 60 tutorials

---
+++ app.js
@@ -0,0 +1,80 @@
+document.write("<h1>wellcome</h1>");
+
+        //Math Methods//
+    //Ceil
+// var a = Math.ceil(2.45);
+// document.write(a);
+
+    //Floor
+// var a = Math.floor(-5.9);
+// document.write(a);
+
+    //Round
+// var a = Math.round(-8.11111111);
+// document.write(a);
+
+    //Trunc
+// var a = Math.trunc(5.936547);
+// document.write(a);
+
+        //Date Methods//
+// var now = new Date();
+// document.write(now);
+
+        //ToDateString
+// var now = new Date(+prompt());
+// document.write(now.toDateString());
+
+        // getDate
+// var mon = new Date();
+// document.write(mon.getDate());
+
+        //getMonth
+// var now = new Date();
+// document.write(now.getMonth());
+
+        //getFullYear
+// var now = new Date();
+// document.write(now.getFullYear());
+
+        // getDay
+// var now = new Date();
+// document.write(now.getDay());
+
+// var now = new Date('august 3 1992');
+// document.write(now.getDay());
+
+// var now = new Date("March 23 1992");
+// document.write(now.getMonth());
+
+// var now = new Date('May 8 1994');
+// document.write(now.getYear());
+
+        //Time Methods
+
+        //getHours
+// var now = new Date();
+// document.write(now.getHours());
+        
+        //getMinutes
+// var now = new Date();
+// document.write(now.getMinutes());
+
+        //getSeconds
+// var now = new Date(); 
+// document.write(now.getSeconds());
+
+        // getMilliseconds
+// var now = new Date();
+// document.write(now.getMilliseconds());
+
+       //setFullyear
+// var now = new Date();
+// now.setFullYear(2023);
+// document.write(now);
+
+       //today Date
+// var today = new Date();
+// document.write(today.getDate() + " / " + today.getMonth() + " / " + today.getFullYear());
+
+  
